854e3a6ad5a16389f5993c59168002a298da88fd
rrd <rrd@1108.cc>
2018-12-27 17:37:24 +0100

    fix whitespace and indentation

--- src/Controller/VisitsController.php
+++ src/Controller/VisitsController.php
@@ -15,7 +15,7 @@ class VisitsController extends AppController
 {
 
 
-	/**
+    /**
      * Index method
      *
      * @return \Cake\Http\Response|void
@@ -26,15 +26,15 @@ class VisitsController extends AppController
             'contain' => ['Clubs']
         ];
         if($this->Auth->user('role') === 'user') {
-	        $visits = $this->paginate( $this->Visits
-		        ->find( 'ByUserId', [ 'user_id' => $this->Auth->user( 'id' ) ] )
-	        );
+            $visits = $this->paginate( $this->Visits
+                ->find( 'ByUserId', [ 'user_id' => $this->Auth->user( 'id' ) ] )
+            );
         }
-	    if($this->Auth->user('role') === 'superuser') {
-		    $visits = $this->paginate( $this->Visits
-			    ->find( 'all' )
-		    );
-	    }
+        if($this->Auth->user('role') === 'superuser') {
+            $visits = $this->paginate( $this->Visits
+                ->find( 'all' )
+            );
+        }
 
         $this->set(compact('visits'));
     }
@@ -51,7 +51,7 @@ class VisitsController extends AppController
         $visit = $this->Visits->get($id, [
             'contain' => ['Clubs', 'ServicesVisits','ServicesVisits.Services']
         ]);
-		$this->checkAccess($visit);
+        $this->checkAccess($visit);
         $this->set('visit', $visit);
     }
 
@@ -63,36 +63,37 @@ class VisitsController extends AppController
     public function add()
     {
         $visit = $this->Visits->newEntity();
-	    $clubs = $this->Visits->Clubs->find('ByUserId',['user_id' => $this->Auth->user('id')])
-	                                 ->find('list', ['limit' => 200]);
+        $clubs = $this->Visits->Clubs
+            ->find('ByUserId', ['user_id' => $this->Auth->user('id')])
+            ->find('list', ['limit' => 200]);
         if ($this->request->is('post')) {
-        	$this->loadModel('ServicesDays');
-	        $visit = $this->Visits->patchEntity( $visit, $this->request->getData() );
-        	if(!$clubs->count()) {
-		        $clubsUser = $this->Visits->Clubs->ClubsUsers->newEntity();
-		        $this->Visits->Clubs->save( $visit->club );
-		        $clubsUser->club_id = $visit->club->id;
-		        $clubsUser->user_id = $this->Auth->user( 'id' );
-		        $this->Visits->Clubs->ClubsUsers->save( $clubsUser );
-	        }
+            $this->loadModel('ServicesDays');
+            $visit = $this->Visits->patchEntity($visit, $this->request->getData());
+            if (!$clubs->count()) {
+                $clubsUser = $this->Visits->Clubs->ClubsUsers->newEntity();
+                $this->Visits->Clubs->save($visit->club);
+                $clubsUser->club_id = $visit->club->id;
+                $clubsUser->user_id = $this->Auth->user('id');
+                $this->Visits->Clubs->ClubsUsers->save($clubsUser);
+            }
             $visit->payed = 0;
-	            if ( $this->Visits->save( $visit ) ) {
-		            $this->Flash->success( __( 'The visit has been saved.' ) );
-		            if(!$clubs->count()){
-			            $email = new Email();
-			            $email->setEmailFormat('html');
-			            $email->setTo($this->Auth->user('email'));
-			            $email->setSubject(__('Add datas for your club') );
-			            $email->setTemplate('clubdata');
-			            $email->setViewVars(['club_id' => $visit->club->id]);
-			            $email->send();
-		            }
+            if ($this->Visits->save($visit)) {
+                $this->Flash->success(__('The visit has been saved.'));
+                if (!$clubs->count()) {
+                    $email = new Email();
+                    $email->setEmailFormat('html');
+                    $email->setTo($this->Auth->user('email'));
+                    $email->setSubject(__('Add datas for your club'));
+                    $email->setTemplate('clubdata');
+                    $email->setViewVars(['club_id' => $visit->club->id]);
+                    $email->send();
+                }
 
-		            return $this->redirect( [ 'action' => 'index' ] );
-	            }
+                return $this->redirect([ 'action' => 'index' ]);
+            }
             $this->Flash->error(__('The visit could not be saved. Please, try again.'));
         }
-        $services = $this->Visits->Services->find('all');
+        $services = $this->Visits->Services->find();
         $this->set(compact('visit', 'clubs', 'services'));
     }
 
@@ -105,24 +106,24 @@ class VisitsController extends AppController
      */
     public function edit($id = null)
     {
-	    $visit = $this->Visits->get($id, [
-		    'contain' => ['Services']
-	    ]);
-	    $this->checkAccess($visit);
-	    $daysDifferent = (strtotime($visit->date) - time()) / (60 * 60 *24);
-	    if($visit->payed == 1 || $daysDifferent <= 7){
-		    $this->Flash->error(__('You can\'t edit this visit yet'));
-		    return $this->redirect(['action' => 'index']);
-	    }
+        $visit = $this->Visits->get($id, [
+            'contain' => ['Services']
+        ]);
+        $this->checkAccess($visit);
+        $daysDifferent = (strtotime($visit->date) - time()) / (60 * 60 *24);
+        if($visit->payed == 1 || $daysDifferent <= 7){
+            $this->Flash->error(__('You can\'t edit this visit yet'));
+            return $this->redirect(['action' => 'index']);
+        }
         if ($this->request->is(['patch', 'post', 'put'])) {
             $visit = $this->Visits->patchEntity($visit,$this->request->getData());
             $visit->payed = 0;
-		        if ( $this->Visits->save( $visit ) ) {
-			        $this->Flash->success( __( 'The visit has been saved.' ) );
+                if ( $this->Visits->save( $visit ) ) {
+                    $this->Flash->success( __( 'The visit has been saved.' ) );
 
-			        return $this->redirect( [ 'action' => 'index' ] );
-		        }
-	        $this->Flash->error(__('The visit could not be saved. Please, try again.'));
+                    return $this->redirect( [ 'action' => 'index' ] );
+                }
+            $this->Flash->error(__('The visit could not be saved. Please, try again.'));
         }
         $club = $this->Visits->Clubs->get($visit->club_id);
         $allServices = $this->Visits->Services->find('all');
@@ -130,7 +131,7 @@ class VisitsController extends AppController
         foreach($allServices as $service){
            $servicesVisits = $this->Visits->ServicesVisits->find('all')->where(['visit_id' => $visit->id, 'service_id' => $service->id])->toArray();
            if($servicesVisits != []){
-           	$service->servicesVisit = $servicesVisits[0];
+               $service->servicesVisit = $servicesVisits[0];
            }
            array_push($services,$service);
         }
@@ -159,18 +160,18 @@ class VisitsController extends AppController
     }
 
     private function checkAccess(Visit $visit){
-	    if($this->Auth->user('role') == 'user') {
-		    $userVisits = $this->Visits->find( 'byUserId', [ 'user_id' => $this->Auth->user( 'id' ) ] );
-		    $canAccess = false;
-		    foreach ($userVisits as $userVisit){
-			    if($visit->id == $userVisit->id){
-				    $canAccess = true;
-			    }
-		    }
-		    if(!$canAccess) {
-			    $this->Flash->error(__('You are not authorized to access that location.'));
-			    $this->redirect( $this->redirect( [ 'action' => 'index' ] ) );
-		    }
-	    }
+        if($this->Auth->user('role') == 'user') {
+            $userVisits = $this->Visits->find( 'byUserId', [ 'user_id' => $this->Auth->user( 'id' ) ] );
+            $canAccess = false;
+            foreach ($userVisits as $userVisit){
+                if($visit->id == $userVisit->id){
+                    $canAccess = true;
+                }
+            }
+            if(!$canAccess) {
+                $this->Flash->error(__('You are not authorized to access that location.'));
+                $this->redirect( $this->redirect( [ 'action' => 'index' ] ) );
+            }
+        }
     }
 }

3a65e225fdf9228b070ad842c795d12e227fb69a
rrd <rrd@1108.cc>
2018-12-27 19:33:10 +0100

    comments and unused code removed

--- webroot/js/visitors.add.js
+++ webroot/js/visitors.add.js
@@ -51,67 +51,7 @@ $(function () {
         $(this).closest('.row').hide().prepend($(this).closest('.column')).fadeIn(1000);
     });
 
-    /*
-        var selectedIds = {};
-        var selectedId = 0;
-        $(".select-service").each(function () {
-            $(this).click(function () {
-                var typeId = $(this).data('type-id');
-                $(".select-service").each(function () {
-                    if ($(this).data('type-id') === typeId) {
-                        if($(this).hasClass('selected')) {
-                            $(this).removeClass('selected');
-                        }
-                    }
-                });
-                if (selectedIds[typeId] !== $(this).data('id')) {
-                    var button = $(this);
-                    selectedId = $(this).data('id');
-                    selectedIds[typeId] = selectedId;
-                    $(".service-data").each(function () {
-                        var dataId = $(this).data('id');
-                        if (dataId !== selectedId && dataId !== 1 && $(this).data('type-id') === typeId) {
-                            var inputs = $(this).find("input");
-                            inputs.each(function () {
-                                if (!$(this).hasClass('service-id')) {
-                                    $(this).remove();
-                                }
-                            });
-                        }
-                    });
-                    var fullPriceMembers = $("#services-1-joindata-full-price-members").val();
-                    var fullPriceSpan = $("span.full-price[data-id='" + selectedId + "']");
-                    fullPriceSpan.append("<input type='hidden' value='" + fullPriceMembers + "' name='services[" + selectedId + "][_joinData][full_price_members]' class='full-price-hidden'>");
-                    var discountPriceMembers = $("#services-1-joindata-discount-price-members").val();
-                    var discountPriceSpan = $("span.discount-price[data-id='" + selectedId + "']");
-                    discountPriceSpan.append("<input type='hidden' value='" + discountPriceMembers + "' name='services[" + selectedId + "][_joinData][discount_price_members]' class='discount-price-hidden'>");
-                    $(this).addClass('selected');
-                } else {
-                    $(".service-data").each(function () {
-                        var dataId = $(this).data('id');
-                        selectedId = 0;
-                        delete selectedIds[typeId];
-                        if (dataId !== 1 && $(this).data('type-id') === typeId) {
-                            var inputs = $(this).find("input");
-                            inputs.each(function () {
-                                if (!$(this).hasClass('service-id')) {
-                                    $(this).remove();
-                                }
-                            });
-                        }
-                    });
-                }
-            });
-        });
-    */
-
-    //price and time calculation
-    $("#services-1-joindata-full-price-members").on("input", function () {
-        $(".full-price-hidden").val($(this).val());
-    });
-    $("#services-1-joindata-discount-price-members").on("input", function () {
-        $(".discount-price-hidden").val($(this).val());
-    });
+    // TODO price and time calculation
 
     $("#send").click(function (event) {
         event.preventDefault();
